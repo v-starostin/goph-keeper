329c504eda10941df6de2098824c924b3abaefef
Viacheslav Starostin <viacheslavstarostin1@yandex.kz>
2024-09-10 00:04:34 +0500

    Initialize DB

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,67 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"net"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 
 	"github.com/v-starostin/goph-keeper/internal/handler"
+	"github.com/v-starostin/goph-keeper/internal/service"
+	"github.com/v-starostin/goph-keeper/internal/storage"
 	"github.com/v-starostin/goph-keeper/pkg/pb"
 )
 
 func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	instance, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://db/migration", "postgres", instance)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err = m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		fmt.Println(err)
+		return
+	}
+
+	authStorage := storage.New(db)
+	authService := service.NewAuth(authStorage, []byte("secret"))
+	authHandler := handler.New(authService)
+
+	server := grpc.NewServer()
+	pb.RegisterAuthServer(server, authHandler)
+
 	l, err := net.Listen("tcp", ":9090")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	authHandler := handler.New(nil)
-
-	server := grpc.NewServer()
-	pb.RegisterAuthServer(server, authHandler)
-
-	if err := server.Serve(l); err != nil {
+	if err = server.Serve(l); err != nil {
 		fmt.Println(err)
 		return
 	}
--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/v-starostin/goph-keeper
 
-go 1.22
+go 1.22.0
+
+toolchain go1.22.2
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/lestrrat-go/jwx v1.2.30
 	github.com/lib/pq v1.10.9
 	google.golang.org/grpc v1.66.0
@@ -12,15 +15,18 @@ require (
 require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.25.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 )
